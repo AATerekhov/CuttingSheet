c944e1a87e8d7f9624b723a9619337dfcea5282a
Оператор 3 < 3@3->
2020-05-05 09:30:49 +0300

    Add project files.

---
+++ RandomlyCuttingSheet/Functions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+using UI = Tekla.Structures.Model.UI;
+
+
+namespace RandomlyCuttingSheet
+{
+    class Functions
+    {
+        #region Creating parts
+        public static void CreatingMainPlate()
+        {
+            var model = new Model();
+
+            if (model.GetConnectionStatus())
+            {
+                var startPoint = PickAPoint();
+                
+                var mainPlate = Plate.MainPlare(startPoint, 6000, 2000);
+                                
+                mainPlate.Insert();
+
+            }
+            model.CommitChanges();
+        }
+
+        public static void CreatingPart()
+        {
+            var model = new Model();
+
+            if (model.GetConnectionStatus())
+            {
+                var startPoint = PickAPoint();
+
+                var rnd = new Random();
+                for (int i = 0; i < rnd.Next(5,15); i++)
+                {
+                    var plate1 = Plate.RandomPoligon(rnd.Next(3, 8), startPoint, rnd.Next(500, 1000), rnd.Next(1000, 2000));
+                    plate1.Insert();
+                    startPoint.X += 1000;
+                }   
+            }
+            model.CommitChanges();
+        }
+
+        public static void CreatingCutting()
+        {
+            var model = new Model();
+
+            if (model.GetConnectionStatus())
+            {
+                var startPoint = PickAPoint();
+
+                var mainPlate = Plate.MainPlare(startPoint, 6000, 2000);
+                mainPlate.Insert();
+
+                var startPointPart = new Point(startPoint.X + 500, startPoint.Y + 1000, startPoint.Z + 6);
+                var rnd = new Random();
+                for (int i = 0; i < rnd.Next(5, 15); i++)
+                {
+                    var plate1 = Plate.RandomPoligon(rnd.Next(3, 8), startPointPart, rnd.Next(500, 1000), rnd.Next(1000, 2000));
+                    plate1.Insert();
+                    startPointPart.X += 1000;
+                }
+
+            }
+            model.CommitChanges();
+        }
+        #endregion
+
+        #region Select an item
+
+        public static Point PickAPoint(string prompt = "Pick a point")
+        {
+            Point myPoint = null;
+            try
+            {
+                var picker = new UI.Picker();
+                myPoint = picker.PickPoint(prompt);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message != "User interrupt")
+                {
+                    Console.WriteLine(ex.Message + ex.StackTrace);
+                }
+            }
+
+            return myPoint;
+        }
+        #endregion
+
+        
+    }
+}
---
+++ RandomlyCuttingSheet/Plate.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+
+
+namespace RandomlyCuttingSheet
+{
+    class Plate
+    {
+        /// <summary>
+        /// Создание прямоугольной пластины
+        /// </summary>
+        /// <param name="startPoint"></param>
+        /// <param name="length"></param>
+        /// <param name="width"></param>
+        /// <returns></returns>
+        public static ContourPlate MainPlare(Point startPoint, double length, double width)
+        {
+            var contourPoint1 = new ContourPoint(startPoint, null);
+            var contourPoint2 = new ContourPoint(new Point(startPoint.X + length, startPoint.Y, startPoint.Z), null);
+            var contourPoint3 = new ContourPoint(new Point(startPoint.X + length, startPoint.Y + width, startPoint.Z), null);
+            var contourPoint4 = new ContourPoint(new Point(startPoint.X, startPoint.Y + width, startPoint.Z), null);
+            
+            var plate = new ContourPlate();
+
+            plate.Name = "Главная пластина";
+            plate.Profile.ProfileString = "—6";
+            plate.Material.MaterialString = "С255";
+            plate.Class = "4";
+            plate.AddContourPoint(contourPoint1);
+            plate.AddContourPoint(contourPoint2);
+            plate.AddContourPoint(contourPoint3);
+            plate.AddContourPoint(contourPoint4);
+            return plate;
+           
+        }
+
+        /// <summary>
+        /// Создание произвольной пластины.
+        /// </summary>
+        /// <param name="num_vertices"></param>
+        /// <param name="startPoint"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static ContourPlate RandomPoligon(int num_vertices, Point startPoint, double width, double height)
+        {
+
+            var rand = new Random();
+            // Выбор случайных радиусов.
+            double[] radii = new double[num_vertices];
+            const int min_radius = 50;
+            const int max_radius = 100;
+            for (int i = 0; i < num_vertices; i++)
+            {
+                radii[i] = rand.Next(min_radius, max_radius)/100.0;
+
+            }
+
+            // Выбор случайных угловых весов.
+            int[] angle_weights = new int[num_vertices];
+            const int min_weight = 1;
+            const int max_weight = 10;
+            double total_weight = 0;
+            for (int i = 0; i < num_vertices; i++)
+            {
+                angle_weights[i] = rand.Next(min_weight, max_weight);
+                total_weight += angle_weights[i];
+            }
+
+            // Преобразование весов во фракции 2 * Pi радианов.
+            double[] angles = new double[num_vertices];
+            double to_radians = 2 * Math.PI / total_weight;
+            for (int i = 0; i < num_vertices; i++)
+            {
+                angles[i] = angle_weights[i] * to_radians;
+            }
+
+            double rx = width / 2;
+            double ry = height / 2;
+            double cx = startPoint.X;
+            double cy = startPoint.Y;
+
+            double theta = 0;
+
+            var listContourPoints = new List<ContourPoint>();
+            for (int i = 0; i < num_vertices; i++)
+            {
+                listContourPoints.Add(new ContourPoint(new Point(cx + (int)(rx * radii[i] * Math.Cos(theta)), cy + (int)(ry * radii[i] * Math.Sin(theta)), startPoint.Z), null));
+                theta += angles[i];
+            }
+            var plate = new ContourPlate();
+            foreach (var point in listContourPoints)
+            {
+                plate.AddContourPoint(point);
+            }
+
+            plate.Name = "Пластина";
+            plate.Profile.ProfileString = "—6";
+            plate.Material.MaterialString = "С255";
+            plate.Class = "6";
+
+            return plate;
+
+        }
+
+
+    }
+}
